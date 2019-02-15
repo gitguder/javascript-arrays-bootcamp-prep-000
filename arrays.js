919e52b70b49fb9567366d6c514be7f9537e5f6e
Codey Pinero <codey.pinero@gmail.com>
2019-02-15 01:46:52 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,27 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray (array, element) {
+ return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element)
+  return array
+  }
+
+function accessElementInArray (array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice()
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
