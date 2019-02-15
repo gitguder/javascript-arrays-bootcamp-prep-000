c7f2b444db072e247cfa3039a1ebacb9b3e905fd
Codey Pinero <codey.pinero@gmail.com>
2019-02-15 01:50:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -25,3 +25,7 @@ function removeElementFromBeginningOfArray(array) {
 function destructivelyRemoveElementFromEndOfArray(array) {
   return array.pop()
 }
+
+function removeElementFromEndOfArray (array) {
+  return array.slice(0, -1)
+}
